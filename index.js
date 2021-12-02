864f42e1c838b91854878fbf5e9147c222a2e8bd
Vikalp Malhotra <kimboho@gmail.com>
2021-12-02 12:47:14 -0500

    finished wiht assignment

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 // Write your solution in this file!
+
+const employee = { name: 'Vik', streetAdress: 'Glendwood Drive' };
+
+
+console.log(employee);
+
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    const newemployee = { ...employee };
+
+    newemployee[key] = value;
+
+    return newemployee;
+
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+
+    employee[key] = value;
+
+    return employee;
+
+}
+
+function deleteFromEmployeeByKey(employee, key) {
+    const p = { ...employee };
+
+    delete p[key];
+
+    return p;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+
+    delete employee[key];
+
+    return employee;
+}
+
+
+
+
+
